e80b6234e99acc5dcfbdbe9d76857db7db269475
root <root@huymac-VirtualBox.(none)>
2014-12-02 03:43:50 -0500

    init

---
+++ conf/settings.py
@@ -0,0 +1,154 @@
+"""
+Django settings for platashop project.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.6/topics/settings/
+
+For the full list of settings and their values, see
+https://docs.djangoproject.com/en/1.6/ref/settings/
+"""
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import os
+from django.utils.translation import ugettext as _
+
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'fwh47btv0u9%8^6el(vn+^dt0)+b5(@g7=$-qv1@*5uaqwd+qk'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+TEMPLATE_DEBUG = True
+
+ALLOWED_HOSTS = []
+
+ADMINS = (
+    # ('Your Name', 'your_email@domain.com'),
+)
+
+MANAGERS = ADMINS
+# Application definition
+
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'plata',
+    'plata.discount',
+    'plata.payment',
+    'plata.product',
+    'plata.shop',
+    'shop',
+)
+
+TEMPLATE_CONTEXT_PROCESSORS = (
+    'django.contrib.auth.context_processors.auth',
+    'django.core.context_processors.debug',
+    'django.core.context_processors.i18n',
+    'django.core.context_processors.media',
+    'django.core.context_processors.request',
+    'django.contrib.messages.context_processors.messages',
+    'plata.context_processors.plata_context',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+ROOT_URLCONF = 'conf.urls'
+
+WSGI_APPLICATION = 'conf.wsgi.application'
+
+
+# Database
+# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+# Internationalization
+# https://docs.djangoproject.com/en/1.6/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+MEDIA_ROOT = os.path.join(os.path.dirname(__file__), 'media/')
+MEDIA_URL = '/media/'
+ADMIN_MEDIA_PREFIX = '/admin_media/'
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.6/howto/static-files/
+
+STATIC_URL = '/static/'
+
+PAYPAL_RESTPAYMENT = {
+    'mode': 'sandbox',
+    'client_id': '...',
+    'client_secret': '...'
+}
+
+
+
+
+# PLATA settings
+
+#PLATA_PRICE_INCLUDES_TAX = False
+
+PLATA_SHOP_CONTACT = 'custom.Contact'
+
+
+POSTFINANCE = {
+    'PSPID': 'plataTEST',
+    'SHA1_IN': 'plataSHA1_IN',
+    'SHA1_OUT': 'plataSHA1_OUT',
+    'LIVE': False,
+    }
+
+PLATA_PAYMENT_MODULE_NAMES = {
+    'paypal': _('Paypal and credit cards'),
+    }
+
+PAYPAL = {
+    'BUSINESS': 'huymac@magrabbit.com',
+    'LIVE': False,
+    }
+
+PLATA_REPORTING_ADDRESSLINE = 'Example Corp. - 3. Example Street - 1234 Example'
+
+TEST_RUNNER = 'options.test_utils.test_runner_with_coverage'
+COVERAGE_MODULES = ['plata']
+
+import logging, sys
+logging.basicConfig(
+    filename='plata.log',
+    format='%(asctime)s %(levelname)s:%(name)s:%(message)s',
+    level=logging.DEBUG,
+    )
+
+PLATA_SHOP_PRODUCT = 'shop.Product'
+CURRENCIES = ('USD',)
---
+++ conf/urls.py
@@ -0,0 +1,26 @@
+import os
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+from django.shortcuts import redirect
+
+from shop.views import shop
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    url(r'^admin/', include(admin.site.urls)),
+
+    url(r'^$', lambda request: redirect('plata_product_list')),
+    url(r'^products/$', 'shop.views.product_list',
+        name='plata_product_list'),
+    url(r'^products/(?P<object_id>\d+)/$', 'shop.views.product_detail',
+        name='plata_product_detail'),
+
+    # url(r'^reporting/', include('plata.reporting.urls')),
+
+    (r'^media/(?P<path>.*)$', 'django.views.static.serve',
+        {'document_root': os.path.join(os.path.dirname(__file__), 'media/')}),
+)
+
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+urlpatterns += staticfiles_urlpatterns()
